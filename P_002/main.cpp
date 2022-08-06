574944d791145a018cc06081a5ea02207c52843d
Bas Man <bas.man.developer@gmail.com>
2022-08-06 16:44:57 +0100

    2nd proj: variables

---
+++ P_002/main.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int num1 = 88;
+	double num2 = 23.12;
+	char ch = 'B';
+	
+
+	cout << num1 << endl;
+	cout << num2 << endl;
+        cout << "character: " << ch  << endl;
+	return 0;	
+}
