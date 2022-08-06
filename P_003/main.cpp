cb9bde60b201f14dfe7df67e1e64905e915bbb83
Bas Man <bas.man.developer@gmail.com>
2022-08-06 17:08:56 +0100

    user input example

---
+++ P_003/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	char a;
+	int num;
+
+	cout << "Enter a char and a number:\n";
+	cin >> a >> num;
+
+	cout << "Character: " << a << endl;
+	cout << "Number: " << num << endl;
+
+	return 0;
+}
